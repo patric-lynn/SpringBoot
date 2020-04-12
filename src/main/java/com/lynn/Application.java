eca9bdf02746361be41eea9e8ba6f75538f5ac2a
lynn <358812977@163.com>
2020-04-13 01:08:17 +0800

    Default Changelist

--- src/main/java/com/lynn/Application.java
+++ src/main/java/com/lynn/Application.java
@@ -1,13 +1,13 @@
-package com.lynn.demo;
+package com.lynn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/com/lynn/DemoApplicationTests.java
+++ src/test/java/com/lynn/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lynn.demo;
+package com.lynn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
